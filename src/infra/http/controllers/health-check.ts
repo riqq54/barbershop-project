9f788c9a5036912b978ddfe6b7e9da7e62e7ad65
Henrique Lima <henriquefreitasl28@gmail.com>
2025-10-09 21:11:05 -0300

    fix: health check controller

--- src/infra/http/controllers/health-check.ts
+++ src/infra/http/controllers/health-check.ts
@@ -2,16 +2,5 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 
 export const healthCheckController: FastifyPluginAsyncZod = async (app) => {
-  app.get(
-    '/health',
-    {
-      schema: {
-        description: 'Health Check Route',
-        response: {
-          200: z.null().describe('ok'),
-        },
-      },
-    },
-    async () => 'ok'
-  )
+  app.get('/health', async () => 'ok')
 }
