c9d1d74cae2ee102d1466fc950e9bb0337d75d52
Henrique Lima <henriquefreitasl28@gmail.com>
2025-10-19 18:08:27 -0300

    add http methods for cors

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -17,6 +17,7 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 })
 
 app.register(fastifySwagger, {
